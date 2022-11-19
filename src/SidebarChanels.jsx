73b1b89d911b34187ece43b61752d1305e153c3e
Emil Mattsson <emilmattsson14@gmail.com>
2022-11-19 15:42:43 +0100

    more styling added

--- src/SidebarChanels.jsx
+++ src/SidebarChanels.jsx
@@ -3,10 +3,12 @@ import "./SidebarChanels.css";
 
 export default function SidebarChanels() {
   return (
-    <div className="sidebar-chanels">
-      <h4>#Gibb</h4>
-      <h4>#Random</h4>
-      <h4>#Batch-3</h4>
+    <div className="sidebar">
+      <div className="sidebar-chanels">
+        <h4>#Gibb</h4>
+        <h4>#Random</h4>
+        <h4>#Batch-3</h4>
+      </div>
     </div>
   );
 }
