f27ddbb43cbfed27642d4f868ded824bed6e11b1
Emil Mattsson <emilmattsson14@gmail.com>
2022-11-21 19:56:42 +0100

    minor change

--- src/components/Sidebar/SidebarChanels.jsx
+++ src/components/Sidebar/SidebarChanels.jsx
@@ -5,15 +5,15 @@ import { Hash } from "react-feather";
 export default function SidebarChanels() {
   return (
     <div className="sidebar-chanels">
-      <h4>
+      <h4 className="channel">
         <Hash />
         gibb
       </h4>
-      <h4>
+      <h4 className="channel">
         <Hash />
         random
       </h4>
-      <h4>
+      <h4 className="channel">
         <Hash />
         batch-3
       </h4>
